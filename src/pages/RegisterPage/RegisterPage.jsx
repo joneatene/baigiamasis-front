f8f04ca40967c8fed7d04a4af4abaf9e8e848b04
joneatene <joneatene@gmail.com>
2021-07-20 14:38:15 +0300

    created Register Page

---
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../../components/Title/Title";
+import Form from "../../components/Form/Form";
+
+const RegisterPage = () => (
+  <>
+    <Title>Register</Title>
+    <Form type="register" />
+  </>
+);
+
+export default RegisterPage;
