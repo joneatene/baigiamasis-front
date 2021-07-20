78dd7ebe73e65ba3328506be4447e216199c70a6
joneatene <joneatene@gmail.com>
2021-07-20 14:31:58 +0300

    created title

---
+++ src/components/Title/Title.style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const TitleBlock = styled.div`
+  text-align: center;
+  padding: 2rem 0;
+`;
+
+export const Title = styled.h2`
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  color: rgb(40, 48, 64);
+`;
