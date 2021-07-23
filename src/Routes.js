44c76380519169125527a07e0883449ba9c89a27
joneatene <joneatene@gmail.com>
2021-07-23 20:28:14 +0300

    updated router

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 
 const Routes = () => (
   <Router>
     <Header />
     <Switch>
-      <Route exact path="/" />
-      <Route exact path="/login" />
+      <Route exact path="/" component={RegisterPage} />
+      <Route exact path="/login" component={LoginPage} />
       <Route exact path="/timeline" />
     </Switch>
   </Router>
--- src/contexts/userContext.js
+++ src/contexts/userContext.js
@@ -21,9 +21,7 @@ const UserProvider = ({ children }) => {
     });
   }, []);
 
-  return (
-    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
-  );
+  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
 };
 
 export default UserProvider;
