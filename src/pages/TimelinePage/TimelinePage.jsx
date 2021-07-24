952499e9ae9f6dd3f90c99e2e98ff6447106d05a
joneatene <joneatene@gmail.com>
2021-07-24 19:35:24 +0300

    post validation

--- src/pages/TimelinePage/TimelinePage.jsx
+++ src/pages/TimelinePage/TimelinePage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 import { UserContext } from "../../contexts/userContext";
+import * as Yup from "yup";
 import * as S from "./Timeline.style";
 import Button from "../../components/Button/Button";
 
@@ -9,6 +10,22 @@ const TimelinePage = () => {
   useEffect(() => setUser(userContext), [userContext]);
 
   if (user) {
+    const validation = (e) => {
+      e.preventDefault();
+      const content = e.target.elements.content.value.trim();
+
+      if (content) {
+        const schema = Yup.object().shape({
+          content: Yup.string().max(255).min(2).required(),
+        });
+
+        schema.isValid({ content }).then((data) => {
+          data ? alert("fetch") : alert("Bad post :(");
+        });
+      } else {
+        alert("Please write something");
+      }
+    };
     return (
       <div className="wrapper">
         <S.UserForm>
@@ -16,9 +33,9 @@ const TimelinePage = () => {
             <S.Photo src={user.photo} />
             <S.Name>{user.fullname}</S.Name>
           </S.InfoBlock>
-          <form>
+          <form onSubmit={validation}>
             <S.Input
-              name="post"
+              name="content"
               minLength="2"
               maxLength="255"
               placeholder={`What's on your mind ${
@@ -26,10 +43,7 @@ const TimelinePage = () => {
               }?`}
               required
             />
-            <Button
-              type="submit"
-              color="primary"
-            >
+            <Button type="submit" color="primary">
               Post
             </Button>
           </form>
